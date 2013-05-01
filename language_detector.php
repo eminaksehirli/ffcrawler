f817fef26add3c31b22c9e97d1cad3a680e26edb
M. Emin Aksehirli <emin.aksehirli@gmail.com>
2013-05-01 03:03:47 +0200

    Very single script to detect languages is added.

---
+++ language_detector.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once('BookDB.php');
+require_once('config.php');
+
+$bookdb = new BookDB();
+
+$db_files = $bookdb->get_filenames();
+$langs = $bookdb->get_languages();
+
+
+$dir = Config::book_download_dir . "/text/";
+
+$files = scandir($dir);
+
+foreach($files as $file)
+{
+	if(strcmp('.', substr($file, 0, 1)) == 0)
+		continue;
+
+	$handle = @fopen($dir . $file, "r");
+
+	$tr = $en = 0;
+	$lineCount = 0;
+	if ($handle)
+	{
+		while (($buffer = fgets($handle, 4096)) !== false)
+		{
+			$lineCount++;
+			if($arr = split(" ", $buffer))
+			{
+				foreach($arr as $word)
+				{
+					if(strcmp(trim($word), "ve") == 0)
+					    $tr++;
+					if(strcmp(trim($word), "and") == 0)
+					    $en++;
+				}
+			}
+		}
+		if (!feof($handle)) {
+		    echo "Error: unexpected fgets() fail\n";
+		}
+		fclose($handle);
+
+		$s = substr($file, 0, -4);
+		$id = $db_files[$s];
+
+		if(!isset($id))
+		    continue;
+
+		//echo("$id, " . substr($file, 0, 10) . " : ");
+
+		if($en > 50 && $tr < 50)
+			$bookdb->update_language($id, $langs['en']);
+		if($tr > 50 && $en < 50)
+			$bookdb->update_language($id, $langs['tr']);
+
+		//echo("\n");
+	}
+}
